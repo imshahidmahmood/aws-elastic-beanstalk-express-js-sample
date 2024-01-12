81f18ae22334cfacacb61d939b96009bfceea51d
shahid mahmood <shahidmahmood.yar@gmail.com>
2024-01-12 15:35:43 +0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Shady, Happy Devops Practice!'));
+app.get('/', (req, res) => res.send('Hello Shady, Full Pipeline Practice!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
